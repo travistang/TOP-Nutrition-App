270aa36f111c6a0bd64c8f61e5c9092cbe1e6a32
Travis Tang <travis.tang@tum.de>
2022-07-12 20:19:29 +0200

    Reduce font size to avoid collision in item widget

--- src/components/SummaryWidgets/ItemWidget.tsx
+++ src/components/SummaryWidgets/ItemWidget.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import classNames from 'classnames';
-import ProgressBar from '../ProgressBar';
+import React from "react";
+import classNames from "classnames";
+import ProgressBar from "../ProgressBar";
 
 type Props = {
   name: string;
@@ -9,20 +9,38 @@ type Props = {
   unit: string;
   className?: string;
   themeColor?: string;
-}
-export default function ItemWidget({ name, value, maxValue, unit, className, themeColor = 'rgb(0,0,0)'}:Props) {
-  const progressBarData = [
-    { name, value, color: themeColor }
-  ];
+};
+export default function ItemWidget({
+  name,
+  value,
+  maxValue,
+  unit,
+  className,
+  themeColor = "rgb(0,0,0)",
+}: Props) {
+  const progressBarData = [{ name, value, color: themeColor }];
 
   return (
-    <div className={classNames('grid grid-cols-6 items-center', className)}>
-      <span className="font-bold leading-3 col-span-2 capitalize" style={{color: themeColor}}>{name}</span>
-      <div className="font-bold col-span-4 text-lg text-right" >
-        <span className="text-lg font-bold" style={{ color: themeColor }}>{value.toFixed(1)}</span>
-        <span className="text-xs text-gray-500">/{maxValue} {unit}</span>
+    <div className={classNames("grid grid-cols-6 items-center", className)}>
+      <span
+        className="font-bold text-sm leading-3 col-span-2 capitalize"
+        style={{ color: themeColor }}
+      >
+        {name}
+      </span>
+      <div className="font-bold col-span-4 text-lg text-right">
+        <span className="text-lg font-bold" style={{ color: themeColor }}>
+          {value.toFixed(1)}
+        </span>
+        <span className="text-xs text-gray-500">
+          /{maxValue} {unit}
+        </span>
       </div>
-      <ProgressBar className="col-span-full h-1" data={progressBarData} totalValue={maxValue} />
+      <ProgressBar
+        className="col-span-full h-1"
+        data={progressBarData}
+        totalValue={maxValue}
+      />
     </div>
-  )
+  );
 }
