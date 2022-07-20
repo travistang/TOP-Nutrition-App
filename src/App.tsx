f6f4e6f49e73d449ffbff7d768846e74d57c4618
Travis Tang <travis.tang@tum.de>
2022-07-20 23:37:08 +0200

    Add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Toaster } from "react-hot-toast";
 import { useRecoilState } from "recoil";
+import { ErrorBoundary } from "@sentry/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import CreateRecordModal from './components/CreateRecordModal';
-import DailyNutritionGoalModal from './components/DailyNutritionGoalModal';
-import Fab from './components/Fab';
-import Header from './components/Header';
-import SplitMealModal from './components/SplitMealModal';
-import ConsumptionSummary from './pages/ConsumptionSummary';
-import { DEFAULT_CONSUMPTION } from './types/Consumption';
+import CreateRecordModal from "./components/CreateRecordModal";
+import DailyNutritionGoalModal from "./components/DailyNutritionGoalModal";
+import Fab from "./components/Fab";
+import Header from "./components/Header";
+import SplitMealModal from "./components/SplitMealModal";
+import ConsumptionSummary from "./pages/ConsumptionSummary";
+import { DEFAULT_CONSUMPTION } from "./types/Consumption";
 import { createEditRecordAtom } from "./atoms/CreateEditRecordAtom";
 import PreviousStatistics from "./pages/PreviousStatistics";
 
@@ -18,31 +19,32 @@ function App() {
     useRecoilState(createEditRecordAtom);
 
   return (
-    <BrowserRouter basename="/TOP-Nutrition-App">
-      <div className="page bg-gray-200">
-        <Toaster />
-        <Header />
-        <Routes>
-          <Route path="/" element={<ConsumptionSummary />} />
-          <Route path="/stats" element={<PreviousStatistics />} />
-        </Routes>
-        {!createEditRecord.modalOpened && (
-          <Fab
-            icon="pen"
-            onClick={() =>
-              setCreatEditRecord({
-                modalOpened: true,
-                record: { ...DEFAULT_CONSUMPTION, date: Date.now() },
-              })
-            }
-          />
-        )}
-        <CreateRecordModal />
-        <DailyNutritionGoalModal />
-        <SplitMealModal />
-
-      </div>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter basename="/TOP-Nutrition-App">
+        <div className="page bg-gray-200">
+          <Toaster />
+          <Header />
+          <Routes>
+            <Route path="/" element={<ConsumptionSummary />} />
+            <Route path="/stats" element={<PreviousStatistics />} />
+          </Routes>
+          {!createEditRecord.modalOpened && (
+            <Fab
+              icon="pen"
+              onClick={() =>
+                setCreatEditRecord({
+                  modalOpened: true,
+                  record: { ...DEFAULT_CONSUMPTION, date: Date.now() },
+                })
+              }
+            />
+          )}
+          <CreateRecordModal />
+          <DailyNutritionGoalModal />
+          <SplitMealModal />
+        </div>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
