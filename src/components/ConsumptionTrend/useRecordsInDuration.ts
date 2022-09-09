1b10c9a2e350cb8d98e836769b2224692c353200
Travis Tang <yiu.tang@c02d80u3md6v.check24.intern>
2022-09-09 11:58:06 +0200

    TNA-29: Fix build

--- src/components/ConsumptionTrend/useRecordsInDuration.ts
+++ src/components/ConsumptionTrend/useRecordsInDuration.ts
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import ConsumptionDatabase, { ConsumptionRecord } from '../../database/ConsumptionDatabase';
 import { baseChartSectionContext } from './BaseChartSection';
 
