9c3fd8d355bcf79af972d0d230c11f821c7dbf74
Travis Tang <yiu.tang@c02d80u3md6v.check24.intern>
2022-10-01 18:50:20 +0200

    Add 2 months duration

--- src/types/Duration.ts
+++ src/types/Duration.ts
@@ -1,5 +1,6 @@
 export enum Duration {
   OneMonth = "1 month",
+  TwoMonths = "2 months",
   ThreeMonths = "3 months",
   HalfYear = "6 months",
   Year = "1 year",
@@ -7,6 +8,7 @@ export enum Duration {
 
 export const DurationToMonthMap: Record<Duration, number> = {
   [Duration.OneMonth]: 1,
+  [Duration.TwoMonths]: 2,
   [Duration.ThreeMonths]: 3,
   [Duration.HalfYear]: 6,
   [Duration.Year]: 12,
