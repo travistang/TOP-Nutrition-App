9ce5fe0a86a29b28c5912b12acb3e69f8bf4314d
Travis Tang <yiu.tang@c02d80u3md6v.check24.intern>
2022-10-09 16:30:28 +0200

    Remove old consumption form

--- src/components/CreateRecordModal/index.tsx
+++ src/components/CreateRecordModal/index.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { DEFAULT_CONSUMPTION } from "../../types/Consumption";
 import { createEditRecordAtom } from "../../atoms/CreateEditRecordAtom";
 
 import Modal from "../Modal";
-import CreateRecordForm from "./CreateRecordForm";
 import StepCreateConsumptionRecordForm from "./StepCreateConsumptionRecordForm";
-import { featureToggleAtom } from "../../atoms/FeatureToggleAtom";
 
 export default function CreateRecordModal() {
-  const { stepConsumptionForm} = useRecoilValue(featureToggleAtom);
   const [createEditRecord, setCreateEditRecord] =
     useRecoilState(createEditRecordAtom);
   const { openingSource, record: consumption } = createEditRecord;
@@ -29,7 +26,7 @@ export default function CreateRecordModal() {
       onClose={onClose}
       label={isEditing ? "Edit record" : "Create nutrition record"}
     >
-      {stepConsumptionForm ? <CreateRecordForm/> : <StepCreateConsumptionRecordForm />}
+      <StepCreateConsumptionRecordForm />
     </Modal>
   );
 }
