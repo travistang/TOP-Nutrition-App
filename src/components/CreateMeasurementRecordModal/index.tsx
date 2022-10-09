306cd1b480a85cba264b7c0c851a9e65e3c28803
Travis Tang <yiu.tang@c02d80u3md6v.check24.intern>
2022-10-09 16:27:55 +0200

    Remove old exercise and measurement forms

--- src/components/CreateMeasurementRecordModal/index.tsx
+++ src/components/CreateMeasurementRecordModal/index.tsx
@@ -4,23 +4,19 @@ import { createMeasurementRecordAtom } from "../../atoms/CreateMeasurementAtom";
 
 import useMeasurementRecordMutation from "./useMeasurementRecordMutation";
 import Modal from "../Modal";
-import { featureToggleAtom } from "../../atoms/FeatureToggleAtom";
-import CreateMeasurementForm from "./CreateMeasurementForm";
 import StepCreateMeasurementRecordForm from "./StepCreateMeasurementRecordForm";
 
 export default function CreateMeasurementRecordModal() {
   const createMeasurementRecord = useRecoilValue(createMeasurementRecordAtom);
-  const featureToggle = useRecoilValue(featureToggleAtom);
   const { onClose } = useMeasurementRecordMutation();
 
   const { modalOpened, record } = createMeasurementRecord;
   const isEditing = !!record.id;
   const modalLabel = isEditing ? "Editing record" : "Record measurement";
 
-  const Form = featureToggle.stepMeasurementForm ? CreateMeasurementForm : StepCreateMeasurementRecordForm;
   return (
     <Modal onClose={onClose} opened={modalOpened} label={modalLabel}>
-      <Form />
+      <StepCreateMeasurementRecordForm />
     </Modal>
   );
 }
