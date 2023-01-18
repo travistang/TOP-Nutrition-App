28789c7422135fbe202b507caa3b03a4a6e0b826
Travis Tang <yiu.tang@c02d80u3md6v.check24.intern>
2023-01-18 14:19:56 +0100

    TNA-84: Measurement record edit and delete

--- src/components/CreateMeasurementRecordModal/StepCreateMeasurementRecordForm/MeasurementPreview.tsx
+++ src/components/CreateMeasurementRecordModal/StepCreateMeasurementRecordForm/MeasurementPreview.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Measurement } from '../../../types/Measurement';
+import SmallNotice from '../../SmallNotice';
 
 type Props = {
-  measurement: Measurement;
+  measurement: Measurement & { id?: string };
 };
 export default function MeasurementPreview({ measurement }: Props) {
   if (!measurement.name) return null;
   const showValue = !!measurement.unit;
+  const isEditing = !!measurement.id;
   return (
-    <div className="flex justify-between pt-2 pb-4">
-      <span className="text-xs font-bold">{measurement.name}</span>
+    <div className="grid grid-cols-6 justify-between pt-2 pb-4 gap-y-2">
+      {isEditing && <SmallNotice className="col-span-full" icon="info-circle">You are editing an existing record</SmallNotice>}
+      <span className="text-xs font-bold col-span-4">{measurement.name}</span>
       {showValue && (
-        <span className="text-xs font-bold">
+        <span className="text-xs font-bold col-span-2 justify-self-end">
           {measurement.value.toFixed(2)}{measurement.unit}
         </span>
       )}
