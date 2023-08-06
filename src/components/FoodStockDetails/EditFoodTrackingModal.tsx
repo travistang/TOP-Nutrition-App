b94b20e92c2649f97bc173ae1dc52207a3341b52
Travis Tang <travis.tang@tum.de>
2023-08-06 10:24:19 +0200

    :ambulance: Fix modal shifting to the right

--- src/components/FoodStockDetails/EditFoodTrackingModal.tsx
+++ src/components/FoodStockDetails/EditFoodTrackingModal.tsx
@@ -43,7 +43,7 @@ export default function EditFoodTrackingModal({ onClose, foodDetail }: Props) {
       label="Edit tracking"
       opened={!!trackingPlaceholder}
       onClose={onClose}
-      className="translate-x-[50%] pr-4"
+      className="pr-4"
     >
       <div className="flex flex-col items-stretch gap-2">
         {trackingPlaceholder && (
