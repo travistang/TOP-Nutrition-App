4ee615a5d0c85608448babc9e8d8f833217a2978
Travis Tang <travis.tang@tum.de>
2023-08-06 11:23:45 +0200

    :sparkles: TNA-112: Cardio workout legend

--- src/components/WorkoutDayTypeWidget/index.tsx
+++ src/components/WorkoutDayTypeWidget/index.tsx
@@ -4,11 +4,23 @@ import { ExerciseDayTypeColorMap } from "../../types/Exercise";
 import Calendar from "../Calendar";
 import DateInput from "../Input/DateInput";
 import { DateInputType } from "../Input/DateInput/types";
-import Legend from "../Legend";
+import Legend, { LegendType } from "../Legend";
 import Section from "../Section";
 import WorkoutOfDayList from "../WorkoutOfDayList";
 import useWorkoutCalendarData from "./useWorkoutCalendarData";
 
+const LEGENDS: { label: string; legendType: LegendType; color: string }[] = [
+  ...Object.entries(ExerciseDayTypeColorMap).map(([dayType, color]) => ({
+    label: dayType,
+    color,
+    legendType: LegendType.Circle,
+  })),
+  {
+    label: "Cardio",
+    color: "black",
+    legendType: LegendType.Dot,
+  },
+];
 export default function WorkoutDayTypeWidget() {
   const [selectedMonth, setSelectedMonth] = useState(new Date());
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -41,9 +53,14 @@ export default function WorkoutDayTypeWidget() {
         selectedDate={selectedDate}
         onSelectDate={setSelectedDate}
       />
-      <div className="flex flex-wrap gap-2 justify-around py-2">
-        {Object.entries(ExerciseDayTypeColorMap).map(([dayType, color]) => (
-          <Legend label={dayType} color={color} key={dayType} />
+      <div className="flex flex-wrap gap-2 justify-evenly py-2">
+        {LEGENDS.map(({ label, color, legendType }) => (
+          <Legend
+            key={label}
+            label={label}
+            color={color}
+            legendType={legendType}
+          />
         ))}
       </div>
       <div className="py-2 max-h-24 overflow-y-auto">
