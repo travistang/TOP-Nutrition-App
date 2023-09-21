c9a4ea1b88c1097487008d3c8d2d4bb1c4beb3fe
Travis Tang <yiu.tang@check24.de>
2023-09-21 21:59:25 +0200

    :construction: TNA-123: WIP: Achievement summary modal

---
+++ src/components/ChallengeSummaryPage/CreateChallengeModal/index.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import Modal from "../../Modal";
+import {
+  CHALLENGE_MODE_SETTINGS,
+  Challenge,
+  ChallengeMode,
+  ChallengePeriod,
+} from "../../../types/Achievement";
+import TextInput from "../../Input/TextInput";
+import { addMonths, endOfDay } from "date-fns";
+import achievementDatabase from "../../../database/AchievementDatabase";
+import { toast } from "react-hot-toast";
+import { Modifier } from "../../../types/utils";
+import Button, { ButtonStyle } from "../../Input/Button";
+import TickerInput from "../../Input/TickerInput";
+import AttributeValueInputToggle from "../../Input/AttributeValueInputToggle";
+
+type Props = {
+  opened: boolean;
+  onClose: () => void;
+};
+
+type ChallengeForm = Omit<Challenge, "id"> & {
+  hasEndDate: boolean;
+};
+const DEFAULT_CHALLENGE: ChallengeForm = {
+  name: "",
+  description: "",
+  mode: ChallengeMode.GreaterThanTarget,
+  target: 0,
+  period: ChallengePeriod.Weekly,
+  hasEndDate: false,
+  endsAt: endOfDay(addMonths(Date.now(), 1)).getTime(),
+};
+
+const isFormValid = (form: ChallengeForm) => {
+  if (!form.name || !form.description || !form.target) return false;
+  return true;
+};
+
+const formToChallengeInput = (form: ChallengeForm): Omit<Challenge, "id"> => {
+  const { endsAt, hasEndDate, ...data } = form;
+  return {
+    ...data,
+    endsAt: hasEndDate ? endsAt : undefined,
+  };
+};
+
+const getNextToggledMode = (currentMode: ChallengeMode): ChallengeMode => {
+  if (currentMode === ChallengeMode.GreaterThanTarget)
+    return ChallengeMode.LessThanTarget;
+  return ChallengeMode.GreaterThanTarget;
+};
+export default function CreateChallengeModal({ onClose, opened }: Props) {
+  const [challenge, setChallenge] = useState<ChallengeForm>(DEFAULT_CHALLENGE);
+  const onCreateChallenge = async () => {
+    if (!isFormValid(challenge)) {
+      toast.error("Invalid form");
+      return;
+    }
+    const challengeInput = formToChallengeInput(challenge);
+    try {
+      await achievementDatabase.createChallenge(challengeInput);
+      toast.success("Challenge created");
+      setChallenge(DEFAULT_CHALLENGE);
+      onClose();
+    } catch {
+      toast.error("Failed to create challenge");
+    }
+  };
+
+  const changeField: Modifier<ChallengeForm> = (field) => (value) => {
+    setChallenge({ ...challenge, [field]: value });
+  };
+  return (
+    <Modal label="Create challenge" opened={opened} onClose={onClose}>
+      <div className="grid grid-cols-6 gap-2">
+        <TextInput
+          label="Challenge name"
+          className="col-span-4"
+          onChange={changeField("name")}
+          value={challenge.name}
+        />
+        <TextInput
+          label="Challenge name"
+          className="col-span-full"
+          onChange={changeField("description")}
+          value={challenge.description}
+        />
+        <AttributeValueInputToggle
+          value={challenge.mode}
+          options={CHALLENGE_MODE_SETTINGS}
+          onToggle={}
+        />
+        <Button
+          buttonStyle={ButtonStyle.Block}
+          onClick={onCreateChallenge}
+          text="Create"
+          icon="plus"
+          className="col-start-4 col-span-3"
+        />
+      </div>
+    </Modal>
+  );
+}
--- src/pages/ChallengeSummary.tsx
+++ src/pages/ChallengeSummary.tsx
@@ -1,14 +1,39 @@
 import classNames from "classnames";
-import StatisticsNavigateTab from "../components/StatisticsNavigateTab";
+import { useState } from "react";
+import { useLiveQuery } from "dexie-react-hooks";
 
+import StatisticsNavigateTab from "../components/StatisticsNavigateTab";
+import Button, { ButtonStyle } from "../components/Input/Button";
+import CreateChallengeModal from "../components/ChallengeSummaryPage/CreateChallengeModal";
+import AchievementDatabase from "../database/AchievementDatabase";
+import EmptyNotice from "../components/EmptyNotice";
 type Props = {
   className?: string;
 };
 export default function ChallengeSummary({ className }: Props) {
+  const challenges = useLiveQuery(() => AchievementDatabase.getAllChallenges());
+  const [showCreateChallengeModal, setShowCreateChallengeModal] =
+    useState(false);
+
+  const noChallenges = challenges && challenges.length === 0;
+
   return (
     <div className={classNames("flex flex-col items-stretch gap-2", className)}>
+      <CreateChallengeModal
+        opened={showCreateChallengeModal}
+        onClose={() => setShowCreateChallengeModal(false)}
+      />
       <StatisticsNavigateTab />
-      WIP
+      <Button
+        onClick={() => setShowCreateChallengeModal(true)}
+        className="self-end w-1/2"
+        buttonStyle={ButtonStyle.Block}
+        icon="plus"
+        text="Create Challenge"
+      />
+      {noChallenges && (
+        <EmptyNotice message="No challenges created" icon="trophy" />
+      )}
     </div>
   );
 }
