1b9f8355809313883f237b9470e8bdab987be501
Travis Tang <travis.tang@tum.de>
2024-07-01 20:53:16 +0200

    :sparkles: TNA-134: QR Scanner in meal prep form

--- src/components/MealPrepModal/MealPrepSummary/FoodContainerSelector/index.tsx
+++ src/components/MealPrepModal/MealPrepSummary/FoodContainerSelector/index.tsx
@@ -1,7 +1,9 @@
 import classNames from "classnames";
 import { useLiveQuery } from "dexie-react-hooks";
+import toast from "react-hot-toast";
 import FoodContainerDatabase from "../../../../database/FoodContainerDatabase";
 import EmptyNotice from "../../../EmptyNotice";
+import QRScanner from "../../../QRScanner";
 import Section from "../../../Section";
 import FoodContainerSelectorItem from "./FoodContainerSelectorItem";
 
@@ -31,9 +33,24 @@ export default function FoodContainerSelector({
     selectedContainersId.length === 0
       ? "Selected containers"
       : `Selected containers (${selectedContainersId.length})`;
+  const toggleSelectionByQrCode = (code: string) => {
+    if (!containers?.find((container) => container.identifier === code)) {
+      toast.error("Unrecognized container");
+    }
+    toggleSelection(code);
+  };
   return (
     <Section
       label={sectionTitle}
+      headerComponent={
+        !hasNoContainers && (
+          <QRScanner
+            onQrCodeDetected={toggleSelectionByQrCode}
+            modalMessage="Toggle container selection by scanning its QR code"
+            modalLabel="Scan container QR Code"
+          />
+        )
+      }
       className={classNames("flex flex-col max-h-64 gap-2", className)}
     >
       <div className="flex-1 min-h-[36px] overflow-y-auto py-2 flex flex-col items-stretch">
