749eaff3673f2f5b6c95cc8c87dfa0627eeb7b66
Travis Tang <travis.tang@tum.de>
2025-09-13 15:55:55 +0200

    Disable exercise details fetch on auto search

--- src/components/ExerciseDetailPage/ExerciseSearchResult/index.tsx
+++ src/components/ExerciseDetailPage/ExerciseSearchResult/index.tsx
@@ -1,47 +1,15 @@
-import { useCallback, useEffect, useState } from "react";
-import { toast } from "react-hot-toast";
-import ExerciseDatabase from "../../../database/ExerciseDatabase";
-import { Exercise, ExerciseDetail } from "../../../types/Exercise";
-import ImagePicker from "../../Input/ImagePicker";
+import { Exercise } from "../../../types/Exercise";
 import Section from "../../Section";
 
 type Props = {
   exercise: Exercise;
 };
 export default function ExerciseSearchResult({ exercise }: Props) {
-  const [exerciseDetail, setExerciseDetail] = useState<ExerciseDetail | undefined>(undefined);
-  useEffect(() => {
-    ExerciseDatabase.findExerciseDetails(exercise).then(setExerciseDetail);
-  }, [exercise]);
-
-  const onChooseImage = useCallback(
-    (image: Blob | null) => {
-      if (!exerciseDetail) return;
-      const newExerciseDetails = {
-        ...exerciseDetail,
-        image: image ?? undefined,
-      };
-      ExerciseDatabase.updateExerciseDetails(newExerciseDetails)
-        .then(() => {
-          toast.success("Exercise details updated");
-        })
-        .catch(() => {
-          toast.error("Failed to update exercise details");
-        });
-    },
-    [exerciseDetail]
-  );
-
   return (
     <>
       <Section label="Exercise information" className="gap-2">
         <div className="flex justify-between items-center">
           <h3 className="font-bold flex-1">{exercise.name}</h3>
-          <ImagePicker
-            className="w-16 h-16"
-            image={exerciseDetail?.image ?? null}
-            onChange={onChooseImage}
-          />
         </div>
       </Section>
     </>
--- src/components/Input/ExerciseAutocompleteResult.tsx
+++ src/components/Input/ExerciseAutocompleteResult.tsx
@@ -1,25 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { useEffect, useState } from "react";
-import ExerciseDatabase from "../../database/ExerciseDatabase";
-import { EquipmentIcon, Exercise, ExerciseDetail } from "../../types/Exercise";
+import { EquipmentIcon, Exercise } from "../../types/Exercise";
 import StringUtils from "../../utils/String";
-import ImageViewer from "../ImageViewer";
 
 type Props = {
   exercise: Exercise;
 };
 
 export default function ExerciseAutocompleteResult({ exercise }: Props) {
-  const [exerciseDetails, setExerciseDetails] = useState<ExerciseDetail | undefined>(undefined);
-
-  useEffect(() => {
-    ExerciseDatabase.findExerciseDetails(exercise).then(setExerciseDetails);
-  }, [exercise]);
-
   return (
     <div className="flex self-center items-center">
-      <ImageViewer className="h-10" image={exerciseDetails?.image ?? null} />
       <div className="flex flex-1 flex-col">
         <div className="font-bold text-sm col-span-5">{exercise.name}</div>
         <span className="text-xs col-span-full leading-4">
